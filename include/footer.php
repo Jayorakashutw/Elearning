29f26ac0508707bfbb224a967c995ead8fcaf6dc
Jay Prakash OSFIGO <jayprakashutw@outlook.com>
2025-06-15 02:33:24 +0530

    IncludeDone

---
+++ include/footer.php
@@ -0,0 +1,75 @@
+<footer class="footer">
+			<div class="container">	
+				<div class="row">
+					<div class="col-lg-3 col-md-6 footer-logo align-self-center wow fadeIn">
+						<div class="footer-widget about-widget ">
+							<img src="assets/img/logo-white.svg" class="fot_logo" alt="Edutec">
+							<p>
+								Lorem ipsum dolor amet consecto adi
+								pisicingelit sed eiusm tempor incidid unt
+								labore dolore.
+							</p>
+
+							<ul class="fsocial-option">								
+								<li><a href="#" class="fb"><i class="fa-brands fa-facebook-f"></i></a></li>
+								<li><a href="#" class="tw"><i class="fa-brands fa-x-twitter"></i></a></li>
+								<li><a href="#" class="lin"><i class="fa-brands fa-linkedin-in"></i></a></li>
+								<li><a href="#" class="yt"><i class="fa-brands fa-youtube"></i></a></li>
+							</ul>							
+						</div>
+					</div><!-- End Col -->	
+
+					<div class="col-lg-3 col-md-6 wow fadeIn">
+						<div class="footer-widget">
+							<h4 class="ftitle">Useful Links</h4>
+							<ul>
+								<li><a href="#">About</a></li>
+								<li><a href="#">Course</a></li>
+								<li><a href="#">Instructor</a></li>
+								<li><a href="#">Events</a></li>
+								<li><a href="#">Instructor Details</a></li>
+								<li><a href="#">Purchase Guide</a></li>
+							</ul>
+						</div>
+					</div><!-- End Col -->	
+
+					<div class="col-lg-3 col-md-6 wow fadeIn">
+						<div class="footer-widget">
+							<h4 class="ftitle">Our Company</h4>
+							<ul>
+								<li><a href="#">Contact Us</a></li>
+								<li><a href="#">Technology</a></li>
+								<li><a href="#">Instructors</a></li>
+								<li><a href="#">Pricing</a></li>
+								<li><a href="#">Service</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+							</ul>
+						</div>
+					</div><!-- End Col -->
+
+					<div class="col-lg-3 col-md-6 wow fadeIn">
+						<div class="footer-widget contact-widget">
+							<h4 class="ftitle">Contact Us</h4>
+							<ul class="fcontact-info">
+								<li><span><i class="fa-solid fa-envelope"></i></span> <p>(405) 987-985 <br> (405) 987-985</p></li>
+								<li><span><i class="fa-solid fa-phone"></i></span> <p>support@example.com <br> support@example.com</p></li>
+								<li><span><i class="fa-solid fa-map"></i></span> <p>Palm Jumeirah, <br> Dubai</p></li>
+							</ul>
+						</div>
+					</div><!-- End Col -->
+					
+				</div>
+			</div>
+
+			<div class="copyright text-center">
+				<div class="container">
+					<p>
+						Copyright 2025 Udesha Technology | Developed By Udesha Technology . All Rights Reserved
+					</p>				
+				</div>
+			</div><!-- End copyright -->
+			
+			<img src="assets/img/icons/graduation-hat.svg" alt="cap" class="fot-cap">
+			<img src="assets/img/icons/idea.svg" alt="lamp" class="fot-lamp">
+			<img src="assets/img/icons/fotshap.svg" alt="lamp" class="fot-shap">
+		</footer>
